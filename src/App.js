c78ef07a06b67929b2670ec3cd273b76b1dec512
izhanfaridi <izhanfaridi@gmail.com>
2022-03-21 15:04:37 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className= 'text-white border border-white-300 rounded-md max-w-md'>
+          hello
+      </div>
+    </>
+  );
+}
+
+export default App;
